34f6e72e43b77dde61f7e61ac9376f629e10d5e3
John Laidler <jlaidler@umass.edu>
2021-08-09 14:05:48 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -12,12 +12,30 @@ def main() :
     
     login(driver, username, password)
     
-    for x in range(0,200):
-        time.sleep(getRandomTime())
-        follow_ten_accounts(driver)
-        driver.get('https://www.instagram.com/' + famous_person_account + '/')
+    follow_total = 0
+    while follow_total < 1000:
+        
+        follow_hour, elapsed_time, start_time = 0, 0, time.time()
+        
+        while elapsed_time < 3600:
+            
+            #sets a limit of 100 followers
+            if(follow_hour > 100):
+                while elapsed_time < 3600:
+                    elapsed_time = time.time() - start_time
+                break
+            
+            time.sleep(getRandomTime())
+            
+            follow_ten_accounts(driver)
+            follow_hour += 10
+            follow_total += 10
+            
+            driver.get('https://www.instagram.com/' + famous_person_account + '/')
+            
+            elapsed_time = time.time() - start_time
     
-    unfollow = Unfollow()
-    unfollow.unfollow_not_following_back()
+    #unfollow = Unfollow()
+    #unfollow.unfollow_not_following_back()
 
 main()
